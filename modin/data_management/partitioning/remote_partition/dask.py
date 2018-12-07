coobas/modin

commit f1fc07cf6a7b2617ec6109789fcb3324d1f7595a

redo apply and get properly

modin/data_management/partitioning/remote_partition/dask.py
@@ -20,7 +20,11 @@ class DaskRemotePartition(BaseRemotePartition):
         Returns:
             The object that was `put`.
         """
-        return self.dask_obj.compute()
+        
+        delayed_call = self.delayed_call
+        self.delayed_call = self.dask_obj
+
+        return self.delayed_call.compute()
 
     def apply(self, func, **kwargs):
         """Apply some callable function to the data in this partition.
@@ -37,7 +41,10 @@ class DaskRemotePartition(BaseRemotePartition):
              applied to it.
         """
         # applies the func lazily
-        return self.__class__(self.dask_obj, (func, kwargs))
+
+        delayed_call = self.delayed_call
+        self.delayed_call = self.dask_obj
+        return self.__class__(dask.delayed(func)(delayed_call, **kwargs))
 
     def add_to_apply_calls(self, func, **kwargs):
         """Add the function to the apply function call stack.

commit 35722b36e9bf8d03fa14a4bc94af92b5c54a37c0

correct the implementation of DaskRemotePartition.get()
(mistake introduced in f1fc07cf6a7b2617ec6109789fcb3324d1f7595a)

modin/data_management/partitioning/remote_partition/dask.py
@@ -26,7 +26,7 @@ class DaskRemotePartition(BaseRemotePartition):
         delayed_call = self.delayed_call
         self.delayed_call = self.dask_obj
 
-        return self.delayed_call.compute()
+        return delayed_call.compute()
 
     def apply(self, func, **kwargs):
         """Apply some callable function to the data in this partition.